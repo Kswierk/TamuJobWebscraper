9df165a6ba34119427785464e6ba09aea62f0d64
Jacob Hamilton <jacobhamilton@tamu.edu>
2020-12-08 15:40:18 -0600

    makes table of all years, added caching, other fixes

--- public/index.js
+++ public/index.js
@@ -7,27 +7,17 @@ document.addEventListener("DOMContentLoaded", () => {
                 method: 'post',
                 body: JSON.stringify(postdata),
                 headers: { 'content-type': 'application/json' }
-            }).then(r => r.text())
+            }).then(r => r.json())
             .then(res => handleResponse(res));
-        /*
-        Array.from(document.querySelectorAll("tr")).forEach(e => e.id.match(/major*) ? console.log(e.children[0].children[0].children[0].children[0].children[0].innerText) : "");
-        console.log(typeof(document.querySelector("#resultstbl > tbody")))
-        .children).map(e => e.id.match(/majorrepeater*g) ? e.children[0].children[0].children[0].children[0].children[0].innerText : e.children[0].children[0]);
-        let domparser = new DOMParser();
-        let doc = domparser.parseFromString(await page.innerHTML(), "text/html");
-        console.log(doc.querySelector("#resultsbl > tbody"));
-        */
     });
 });
 
 function handleResponse(res) {
     document.getElementById("btn").disabled = false;
-    let domparser = new DOMParser();
-    //let doc = domparser.parseFromString(res, "text/html");
-    //let majorsData = Array.from(doc.querySelector("#resultstbl > tbody").children).map(e => e.id.match(/majorrepeater*/g) ? e.children[0].children[0].children[0].children[0].children[0].innerText.trim() : e.children[0].children[0]);
 
-    //use jsdom in the backend so we can group all of the major together and send it all back. 
-    //console.log(majors[majors.findIndex(str => str.includes(document.querySelector(".entry").value))]);
-    //document.getElementById("tableContainer").appendChild(majors[majors.indexOf(document.querySelector(".entry").value) + 1]);
-    console.log(res);
+    document.getElementById("tableContainer").innerHTML = makeTableHTML(res);
+}
+
+function makeTableHTML(ar) {
+    return `<table><thead><th>Company</th><th>Job type</th><th>Year</th></thead>${ar.reduce((c, o) => c += `<tr>${o.reduce((c, d) => (c += `<td>${d}</td>`), '')}</tr>`, '')}</table>`;
 }
--- server.js
+++ server.js
@@ -8,6 +8,15 @@ const app = express();
 
 const port = 8000;
 
+const letToSemester = {
+    "A": "Spring",
+    "B": "Summer",
+    "C": "Fall"
+}
+
+let flatCache = require('flat-cache');
+let cache = flatCache.load('server');
+
 app.use(express.json());
 
 app.use(express.static('F:/dev/Tamu CS hiring data/public/'));
@@ -16,14 +25,21 @@ router.get('/', (req, res) => {
     res.sendFile("F:/dev/Tamu CS hiring data/public/index.html");
 });
 
-async function getMajorData(request, browser, semester) {
+async function getYearDataForMajor(browser, request, sem) {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto('https://aggiesurveys.tamu.edu/public/Reports.aspx');
-    await page.selectOption('#ddlWHSemester', semester);
+    await Promise.all([
+        page.selectOption('#ddlWHSemester', sem),
+        page.waitForNavigation({ waitUntil: 'load' })
+    ]);
+    //await new Promise(r => setTimeout(r, 2000));
 
-    await page.waitForLoadState('networkidle');
-    await page.selectOption('#ddlWHCollege', request.body[0]);
+    let val = await page.selectOption('#ddlWHCollege', request.body[0]);
+    if (val.length == 0) {
+        await page.selectOption('#ddlWHCollege', request.body[0] + " ");
+    }
+
     const [newPage] = await Promise.all([
         context.waitForEvent('page'),
         page.click('#btnWHSelect')
@@ -36,20 +52,46 @@ async function getMajorData(request, browser, semester) {
 
     const dom = new jsdom.JSDOM(await newPage.content());
     let majorsData = Array.from(dom.window.document.querySelector("#resultstbl > tbody").children).map(e => e.id.match(/majorrepeater*/g) ? e.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML.trim().replace("<b>Major: </b>", "").replace("&amp;", "&") : e.children[0].children[0]);
-    return majorsData[majorsData.indexOf(request.body[1]) + 1];
+    let majorTable = majorsData[majorsData.indexOf(request.body[1]) + 1];
+    let majorData = Array.from(majorTable.children[0].children).slice(1, -1).map(el => Array.from(el.children).concat([sem.substr(0, 4) + " " + letToSemester[sem.slice(-1)]]).map((ele, i) => i > 1 ? ele : ele.innerHTML));
+    await newPage.close();
+    await context.close();
+    return majorData;
+}
+
+async function getMajorData(request, browser) {
+
+
+    let htmlResp = await (await fetch("https://aggiesurveys.tamu.edu/public/Reports.aspx")).text();
+    const dom = new jsdom.JSDOM(htmlResp);
+    let semesters = Array.from(dom.window.document.querySelector("#ddlWHSemester").children).map(el => el.value).slice(1);
+    let majorData = [];
+    let temp = [];
+    let threads = 18;
+    while (semesters.length > 0) {
+        temp = semesters.splice(0, threads);
+        console.log(temp);
+        (await Promise.all(temp.map(el => getYearDataForMajor(browser, request, el)))).forEach(e => majorData.push(...e));
+    }
+
+    return majorData;
 }
 
 router.post('/send', (request, response) => {
     (async() => {
         console.log(request.body);
+        if (cache.getKey(request.body) !== undefined) {
+            response.set('Content-Type', 'text/html');
+            response.send(cache.getKey(request.body));
+            return;
+        }
         const browser = await firefox.launch();
-        let majorData = await getMajorData(request, browser, "2019C");
+        let majorData = await getMajorData(request, browser);
 
-        console.log(Array.from(majorData.children[0].children).map(el => Array.from(el.children).map(ele => ele.innerHTML)));
-
+        cache.setKey(request.body, majorData);
+        cache.save();
         response.set('Content-Type', 'text/html');
-        response.send(true);
-        //response.end("yes");
+        response.send(majorData);
         //await new Promise(r => setTimeout(r, 100));
         await browser.close();
     })();
