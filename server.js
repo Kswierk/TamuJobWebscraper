55d6bb1cc2808ef62f5ff4934bb7fd83b4468a6c
Ubuntu <ubuntu@ip-172-31-17-96.us-east-2.compute.internal>
2020-12-21 03:41:29 +0000

    debug

--- server.js
+++ server.js
@@ -25,8 +25,7 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-async function getYearDataForMajor(browser, request, sem) {
-    let context = await browser.newContext();
+async function getYearDataForMajor(context, request, sem) {
     let page = await context.newPage();
     await page.goto('https://aggiesurveys.tamu.edu/public/Reports.aspx');
     await Promise.all([
@@ -57,11 +56,12 @@ async function getYearDataForMajor(browser, request, sem) {
 
     await page.close();
 
-    //await newPage.waitForLoadState();
+    await newPage.waitForLoadState();
 
     console.log(await newPage.title());
 
     let dom = new jsdom.JSDOM(await newPage.content());
+    console.log(dom.window.document.querySelector("#resultstbl > tbody"));
     //array alternating from major name to a html table of jobs
     let majorsData = Array.from(dom.window.document.querySelector("#resultstbl > tbody").children).map(e => e.id.match(/majorrepeater*/g) ? e.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML.trim().replace("<b>Major: </b>", "").replace("&amp;", "&") : e.children[0].children[0]);
     //returns html table of the major
@@ -73,30 +73,29 @@ async function getYearDataForMajor(browser, request, sem) {
     //makes 2d array out of table
     let majorData = Array.from(majorTable.children[0].children).slice(1, -1).map(el => Array.from(el.children).concat([sem.substr(0, 4) + " " + letToSemester[sem.slice(-1)]]).map((ele, i) => i > 1 ? ele : ele.innerHTML == "" ? "(Blank)" : ele.innerHTML));
     await newPage.close();
-    await context.close();
     return majorData;
 }
 
 async function getMajorData(request, browser) {
 
-
+    let context = await browser.newContext();
     let htmlResp = await (await fetch("https://aggiesurveys.tamu.edu/public/Reports.aspx")).text();
     let dom = new jsdom.JSDOM(htmlResp);
     let semesters = Array.from(dom.window.document.querySelector("#ddlWHSemester").children).map(el => el.value).slice(1);
     let majorData = [];
     let temp = [];
-    let threads = 1;
+    let threads = 4;
     while (semesters.length > 0) {
         temp = semesters.splice(0, threads);
         console.log(temp);
         try {
-            (await Promise.all(temp.map(el => getYearDataForMajor(browser, request, el)))).forEach(e => e.length != 0 ? majorData.push(...e) : "");
+            (await Promise.all(temp.map(el => getYearDataForMajor(context, request, el)))).forEach(e => e.length != 0 ? majorData.push(...e) : "");
         } catch (e) {
             throw (e);
         }
 
     }
-
+    await context.close();
     return majorData;
 }
 
