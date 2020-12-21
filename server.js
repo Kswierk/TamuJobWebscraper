fbd807ebae11f1d6425467c2d418752bbf3cad6e
Jacob Hamilton <jacobhamilton@tamu.edu>
2020-12-20 21:58:05 -0600

    debug

--- server.js
+++ server.js
@@ -25,7 +25,20 @@ router.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-async function getYearDataForMajor(context, request, sem) {
+Promise.delay = function(t, val) {
+    return new Promise(resolve => {
+        setTimeout(resolve.bind(null, val), t);
+    });
+}
+
+Promise.raceAll = function(promises, timeoutTime, timeoutVal) {
+    return Promise.all(promises.map(p => {
+        return Promise.race([p, Promise.delay(timeoutTime, timeoutVal)])
+    }));
+}
+
+async function getYearDataForMajor(browser, request, sem) {
+    let context = await browser.newContext();
     let page = await context.newPage();
     await page.goto('https://aggiesurveys.tamu.edu/public/Reports.aspx');
     await Promise.all([
@@ -43,16 +56,13 @@ async function getYearDataForMajor(context, request, sem) {
     }
     //await new Promise(r => setTimeout(r, 500));
     //console.log(val);
-    await page.screenshot({ path: 'my_screenshot.png', fullPage: true });
+    //await page.screenshot({ path: 'my_screenshot.png', fullPage: true });
     let newPage;
-    try {
-        [newPage] = await Promise.all([
-            context.waitForEvent('page', { setTimeout: 0 }),
-            (await page.$('#btnWHSelect')).click()
-        ]);
-    } catch (e) {
-        throw (e);
-    }
+    [newPage, isError] = await Promise.raceAll([
+        context.waitForEvent('page'),
+        (await page.$('#btnWHSelect')).click()
+    ], 30000, null);
+    console.log(isError);
 
     await page.close();
 
@@ -61,7 +71,6 @@ async function getYearDataForMajor(context, request, sem) {
     console.log(await newPage.title());
 
     let dom = new jsdom.JSDOM(await newPage.content());
-    console.log(dom.window.document.querySelector("#resultstbl > tbody"));
     //array alternating from major name to a html table of jobs
     let majorsData = Array.from(dom.window.document.querySelector("#resultstbl > tbody").children).map(e => e.id.match(/majorrepeater*/g) ? e.children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].children[0].innerHTML.trim().replace("<b>Major: </b>", "").replace("&amp;", "&") : e.children[0].children[0]);
     //returns html table of the major
@@ -73,12 +82,13 @@ async function getYearDataForMajor(context, request, sem) {
     //makes 2d array out of table
     let majorData = Array.from(majorTable.children[0].children).slice(1, -1).map(el => Array.from(el.children).concat([sem.substr(0, 4) + " " + letToSemester[sem.slice(-1)]]).map((ele, i) => i > 1 ? ele : ele.innerHTML == "" ? "(Blank)" : ele.innerHTML));
     await newPage.close();
+    await context.close();
     return majorData;
 }
 
 async function getMajorData(request, browser) {
 
-    let context = await browser.newContext();
+
     let htmlResp = await (await fetch("https://aggiesurveys.tamu.edu/public/Reports.aspx")).text();
     let dom = new jsdom.JSDOM(htmlResp);
     let semesters = Array.from(dom.window.document.querySelector("#ddlWHSemester").children).map(el => el.value).slice(1);
@@ -89,13 +99,13 @@ async function getMajorData(request, browser) {
         temp = semesters.splice(0, threads);
         console.log(temp);
         try {
-            (await Promise.all(temp.map(el => getYearDataForMajor(context, request, el)))).forEach(e => e.length != 0 ? majorData.push(...e) : "");
+            (await Promise.all(temp.map(el => getYearDataForMajor(browser, request, el)))).forEach(e => e.length != 0 ? majorData.push(...e) : "");
         } catch (e) {
             throw (e);
         }
 
     }
-    await context.close();
+
     return majorData;
 }
 
